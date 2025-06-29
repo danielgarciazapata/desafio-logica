38e4862284c71cdec4c1af249b320184c972a4c9
danielgarciazapata <daniel_zg_lima@yahoo.com.br>
2025-06-29 12:48:58 -0300

    primeiro

---
+++ primeiro_projeto_node.js
@@ -0,0 +1,22 @@
+let Nome = "fulano"
+let NivelDeHeroi = 9000
+
+if(NivelDeHeroi <= 1000){
+    console.log("O Herói de nome " +Nome+ " está no nível de Ferro")
+} else if(NivelDeHeroi >= 1001 && NivelDeHeroi <= 2000){
+    console.log("O Herói de nome " +Nome+ " está no nível de Prata")
+} else if(NivelDeHeroi >= 2001 && NivelDeHeroi <= 5000){
+    console.log("O Herói de nome " +Nome+ " está no nível de Bronze")
+} else if(NivelDeHeroi >= 5001 && NivelDeHeroi <= 7000){
+    console.log("O Herói de nome " +Nome+ " está no nível de Ouro")
+} else if(NivelDeHeroi >= 7001 && NivelDeHeroi <= 8000){
+    console.log("O Herói de nome " +Nome+ " está no nível de Platina")
+} else if(NivelDeHeroi >= 8001 && NivelDeHeroi <= 9000){
+    console.log("O Herói de nome " +Nome+ " está no nível de Ascendente")
+} else if(NivelDeHeroi >= 9001 && NivelDeHeroi <= 10000){
+    console.log("O Herói de nome " +Nome+ " está no nível de Imortal")
+} else if(NivelDeHeroi >= 10001){
+    console.log("O Herói de nome " +Nome+ " está no nível de Radiante")
+}
+
+
